82f795d68898a02fc91b5de2d5bea31487410879
zhal2810 <afrizalafandi3090@gmail.com>
2026-07-03 00:09:44 +0700

    Delete warera-frontend/src directory

--- warera-frontend/src/components/InventoryPanel.jsx
+++
@@ -1,7 +0,0 @@
-// Di dalam komponen InventoryPanel.jsx
-const fetchInventory = async () => {
-  const res = await fetchWarera('inventory.fetchCurrentEquipment', { 
-    userId: userId 
-  });
-  setInventory(res.data);
-};
